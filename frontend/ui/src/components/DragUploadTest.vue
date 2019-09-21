eaed9b8335d66a5ffad0101a33a5a669283c7802
Joey <yingchi1994@gmail.com>
2019-09-22 01:07:51 +0800

    feat: 前端服务列表，拖拽上传配置清单文件

--- frontend/ui/src/components/DragUploadTest.vue
+++ frontend/ui/src/components/DragUploadTest.vue
@@ -2,14 +2,12 @@
     <div>
         <h2>拖拽上传</h2>
         <a-upload-dragger name="file" :multiple="true" action="http://localhost:8888/service" @change="handleChange">
-<!--        <a-upload-dragger name="file" :multiple="true" action="https://www.mocky.io/v2/5cc8019d300000980a055e76" @change="handleChange">-->
             <p class="ant-upload-drag-icon">
                 <a-icon type="inbox" />
             </p>
             <p class="ant-upload-text">Click or drag file to this area to upload</p>
             <p class="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
         </a-upload-dragger>
-        <input type="file" :id="id" name="image" class="getImgUrl_file" @change="preview($event)">
     </div>
 </template>
 <script>
@@ -31,23 +29,6 @@
                 } else if (status === 'error') {
                     this.$message.error(`${info.file.name} 上传失败`);
                 }
-            },
-            preview(event){
-                let files = document.getElementById(this.id).files[0];
-                var reader = new FileReader();
-                let res = reader.readAsText(this.getObjectURL(files))
-                console.log(res)
-            },
-            getObjectURL (file) {
-                let url = null ;
-                if (window.createObjectURL!=undefined) { // basic
-                    url = window.createObjectURL(file) ;
-                }else if (window.webkitURL!=undefined) { // webkit or chrome
-                    url = window.webkitURL.createObjectURL(file) ;
-                }else if (window.URL!=undefined) { // mozilla(firefox)
-                    url = window.URL.createObjectURL(file) ;
-                }
-                return url ;
             },
         },
     }
--- frontend/ui/src/views/DependencyManagement/DataMaintanance.vue
+++ frontend/ui/src/views/DependencyManagement/DataMaintanance.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="service-list">
 
-        <a-button type="primary" style="margin: 20px">上传依赖配置清单</a-button>
+        <a-button type="primary" style="margin: 20px" @click="showDrawer">上传依赖配置清单</a-button>
 
         <a-table :columns="columns" :dataSource="data">
             <a slot="name" slot-scope="text" href="javascript:;">{{text}}</a>
@@ -14,6 +14,23 @@
               <a-divider type="vertical" />
             </span>
         </a-table>
+
+        <a-drawer
+                title="上传依赖配置清单文件"
+                placement="right"
+                :width="500"
+                :closable="false"
+                @close="onClose"
+                :visible="drawer_visible"
+        >
+            <a-upload-dragger name="file" :multiple="true" action="http://localhost:8888/service" @change="handleChange">
+                <p class="ant-upload-drag-icon">
+                    <a-icon type="inbox" />
+                </p>
+                <p class="ant-upload-text">点击此处或拖拽依赖配置文件到此处上传</p>
+                <p class="ant-upload-hint">支持单个文件、多个文件上传</p>
+            </a-upload-dragger>
+        </a-drawer>
     </div>
 </template>
 <script>
@@ -45,6 +62,7 @@
         data() {
             return {
                 data: [],
+                drawer_visible: false,
                 columns,
             }
         },
@@ -58,7 +76,24 @@
                 }).catch((err)=>{
                     console.log("无法绘制 nodes 数据: " + err)
                 });
-            }
+            },
+            showDrawer() {
+                this.drawer_visible = true
+            },
+            onClose() {
+                this.drawer_visible = false
+            },
+            handleChange(info) {
+                const status = info.file.status;
+                if (status !== 'uploading') {
+                    console.log(info.file, info.fileList);
+                }
+                if (status === 'done') {
+                    this.$message.success(`${info.file.name} 上传成功`);
+                } else if (status === 'error') {
+                    this.$message.error(`${info.file.name} 上传失败`);
+                }
+            },
         },
         mounted() {
             this.fetchData();
